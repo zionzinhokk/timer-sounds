cd6b172506b18159a9047deaf2f5438be60b404d
zionkklol <thiagodias152096@gmail.com>
2026-02-19 00:40:26 -0300

    Troca da porta do Server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
